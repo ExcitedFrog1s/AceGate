12182f3f20c734720cbeb4387280521e29ce5bf8
Gatlin1111 <deng.haoyue@outlook.com>
2022-11-14 21:36:57 +0800

    test

--- src/routes/default_router.js
+++ src/routes/default_router.js
@@ -3,12 +3,19 @@ import {
 } from "react-router-dom";
 
 import Landing from '../pages/landing/landing.js'
-
+import AdvancedSearch from '../pages/advancedSearch/advancedSearch.js'
 const default_router = createBrowserRouter([
     {
         path: '/',
         element: <Landing />
+    },
+    {
+        path: '/advancedSearch',
+        element: <AdvancedSearch />
     }
+
 ])
 
 export default default_router;
+
+
