d37ebd686efa3778898e7f7e26149ef39856789b
sunyifan <1964098644@qq.com>
2022-11-18 14:06:20 +0800

    后台管理页面样式完成

--- src/routes/default_router.js
+++ src/routes/default_router.js
@@ -3,11 +3,33 @@ import {
 } from "react-router-dom";
 
 import Landing from '../pages/landing/landing.js'
+import Manage from "../manage/left.js";
+import Info from "../manage/info.js";
+import Scholars from "../manage/scholar.js";
+import Check from "../manage/check.js";
 
 const default_router = createBrowserRouter([
     {
         path: '/',
         element: <Landing />
+    },
+    {
+        path: '/manage',
+        element: <Manage />,
+        children: [
+            {
+                path: 'info',
+                element: <Info />
+            },
+            {
+                path: 'scholars',
+                element: <Scholars />
+            },
+            {
+                path: 'check',
+                element: <Check />
+            }
+        ]
     }
 ])
 
