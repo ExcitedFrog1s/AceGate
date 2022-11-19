3c76a654386bb86d8c65e0718cb0d1b73799ba6c
Will Frog <froggywch@gmail.com>
2022-11-20 00:39:05 +0800

    Axios settings

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {ChakraProvider} from "@chakra-ui/react";
 import 'antd/dist/antd.min.css';
+import axios from 'axios';
+
 
 import {
     RouterProvider,
 } from "react-router-dom";
 
 import default_router from "./routes/default_router";
+
+axios.defaults.baseURL = 'http://localhost:8080'
+axios.defaults.headers.common['Authorization'] = 'AUTH_TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
