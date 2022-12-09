c480f527aa4567351b85f3a1cc6d161a5301e275
WGX <2690121951@qq.com>
2022-12-09 22:48:15 +0800

    1

--- src/pages/personalinf/Edit/index.js
+++ src/pages/personalinf/Edit/index.js
@@ -1,5 +1,5 @@
 //by wgx
-import { Button } from 'antd';
+import { Button, Form, Input } from 'antd';
 import {nanoid} from 'nanoid';
 import Item from '../Item';
 import Select from '../Select'
@@ -7,12 +7,14 @@ import {Link} from 'react-router-dom';
 import React, { Component } from 'react'
 import Interest from '../Interest';
 import {RollbackOutlined, CheckCircleOutlined} from '@ant-design/icons';
+import axios from 'axios';
 
 export default class Edit extends Component {
   state = {fields:[
+    'test'
   ],
   interests:[
-
+    'tst'
   ],
   }
 
@@ -63,6 +65,24 @@ export default class Edit extends Component {
     const newinterests = [...interests,newinterest]
     this.setState({interests:newinterests})
   }
+
+
+  changeInfo = () =>{
+    axios({
+      method: 'POST',
+      url: 'https://mock.apifox.cn/m1/1955876-0-default/personInfo/edit',
+      data:{
+        Rname:'hh',
+        Rcontact: '1',
+        Rinstitute: '2',
+        Uemail: '3',
+        Ufield: ['1', '2', '3'],
+        Uinterest: ['1', '2', '3']
+      }
+    }).then(response =>{
+      console.log(response)
+    });
+  }
   
   render() {
     const saveStyle = {
@@ -70,6 +90,18 @@ export default class Edit extends Component {
       border: 'none',
       boxShadow: '4px 4px 15px 0 rgba(0,0,0,0.2)',
     }
+
+    const validateMessages = {
+      required: '${label}为必填项',
+      types: {
+          email: '请输入有效的${label}!',
+          number: '${label} is not a valid number!',
+      },
+      number: {
+          range: '${label} must be between ${min} and ${max}',
+      },
+    };
+
     const {accountinfs} = this.props
     const {interestwords} = this.props
     const {scholar} = this.props
@@ -87,109 +119,158 @@ export default class Edit extends Component {
             })
           }
         </div>
-        <div className="informationframe">
-          <h2 className="frametitle">个人信息</h2>
-          {scholar
-           ?  <div>
-                <div className="list_container">
-                  <div className="information_name">
-                    <span>真实姓名：</span>
+        <Form>
+          <div className="informationframe">
+            <h2 className="frametitle">个人信息</h2>
+            {scholar
+            ?  <div>
+                  <div className="list_container">
+                    <div className="information_name">
+                    </div>
+                    <div className="infinput">
+                      <Form.Item
+                          name={['user', 'search-area']}
+                          label= {<span>真实姓名</span>}
+                          rules={[
+                              {
+                                  required: false,
+                              },
+                          ]}
+                      >
+                          <Input maxLength="15" className="input1" type="text" placeholder="请输入真实姓名"/>
+                      </Form.Item>
+                    </div>
                   </div>
-                  <div className="infinput">
-                    <input maxlength="15" className="input1" type="text" placeholder="请输入真实姓名"/>
+                  <div className="list_container">
+                    <div className="information_name">
+                    </div>
+                    <div className="infinput">
+                      <Form.Item
+                          name={['user', 'search-area']}
+                          label= {<span>联系电话</span>}
+                          rules={[
+                              {
+                                  required: false,
+                              },
+                          ]}
+                      >
+                          <Input maxLength="20" className="input1" type="text" placeholder="请输入联系电话"/>
+                      </Form.Item>
+                    </div>
+                  </div>
+                  <div className="list_container">
+                    <div className="information_name">
+                    </div>
+                    <div className="infinput">
+                     <Form.Item
+                          label= { <span>工作单位</span>}
+                          rules={[
+                              {
+                                  required: false,
+                              },
+                          ]}
+                      >
+                          <Input maxLength="20" className="input1" type="text" placeholder="请输入工作单位"/>
+                      </Form.Item>
+                    </div>
                   </div>
                 </div>
-                <div className="list_container">
-                  <div className="information_name">
-                    <span>联系电话：</span>
+            :  <div/>
+            }
+            <div className="list_container">
+              <div className="infinput">
+              <Form.Item
+                label={
+                  <div>
+                    <b style={{color: 'rgb(215, 38, 31)'}}>* </b>
+                    <span>E-mail</span>
                   </div>
-                  <div className="infinput">
-                    <input maxlength="20" className="input1" type="text" placeholder="请输入联系电话"/>
-                  </div>
-                </div>
-                <div className="list_container">
-                  <div className="information_name">
-                    <span>工作单位：</span>
-                  </div>
-                  <div className="infinput">
-                    <input maxlength="20" className="input1" type="text" placeholder="请输入工作单位"/>
-                  </div>
-                </div>
-              </div>
-           :  <div/>
-          }
-          <div className="list_container">
-            <div className="information_name">
-              <b style={{color: 'rgb(215, 38, 31)'}}>* </b>
-              <span>E-mail：</span>
-            </div>
-            <div className="infinput">
-              <input maxlength="20" className="input1" type="text" placeholder="请输入E-mail"/>
-            </div>
-          </div>
-          <div className="list_container">
-            <div className="information_name">
-              <span>研究领域：</span>
-            </div>
-            <div className="infinput">
-              <input maxlength="15" ref={c => this.field = c} className="input1" type="text" placeholder="请输入研究领域，点击按钮可完成添加"/>
-              <Button className="clickconfirm" onClick={this.addfield}> √ </Button>
-            </div>
-            <div className='select_wrap'>
-              {
-                fields.map( inf =>{
-                return<Select deletefieldint={this.deletefieldint} key={inf.id} {...inf}/>
-               })
-              }  
-            </div>
-          </div>
-          <div className="list_container">
-            <div className="information_name">
-              <span>我的兴趣词：</span>
-            </div>
-            <div className="infinput">
-              <input maxlength="15" ref={c => this.interest = c} className="input1" type="text" placeholder="请输入兴趣词，点击按钮可完成添加"/>
-              <Button className="clickconfirm" onClick={this.addinterest}> √ </Button>
-            </div>
-            <div className='select_wrap'>
-              {
-                interests.map( inf =>{
-                return<Select deletefieldint={this.deletefieldint} key={inf.id} {...inf}/>
-               })
-              }  
-            </div>
-            <div className="option">
-            <div className="optiontitle">
-                <p>推荐：</p>
-                {
-                  interestwords.map( inf =>{
-                    return<Interest addinterest={this.selectinterest} key={inf.id} {...inf}/>
-                     })
                 }
+                rules={[
+                    {
+                        type: 'email',
+                        required: false,
+                    },
+                ]}
+              >
+                <Input maxLength="20" className="input1" type="text" placeholder="请输入E-mail"/>
+              </Form.Item>
+              </div>
+            </div>
+            <div className="list_container">
+              <div className="information_name">
+              </div>
+              <div className="infinput">
+                <Form.Item
+                    label= {<span>研究领域</span>}
+                    rules={[
+                        {
+                            required: false,
+                        },
+                    ]}
+                >
+                      <Input maxLength="15" ref={c => this.field = c} className="input1" type="text" placeholder="请输入研究领域，点击按钮可完成添加"/>
+                </Form.Item>
+                <Button className="clickconfirm" onClick={this.addfield}> √ </Button>
+              </div>
+              <div className='select_wrap'>
+              <div>hello</div>
+                {
+                  fields.map( inf =>{
+                  return<Select deletefieldint={this.deletefieldint} key={inf.id} {...inf}/>
+                })
+                }  
+              </div>
+            </div>
+            <div className="list_container">
+              <div className="information_name">
+                <span>我的兴趣词：</span>
+              </div>
+              <div className="infinput">
+                <input maxLength="15" ref={c => this.interest = c} className="input1" type="text" placeholder="请输入兴趣词，点击按钮可完成添加"/>
+                <Button className="clickconfirm" onClick={this.addinterest}> √ </Button>
+              </div>
+              <div className='select_wrap'>
+                {
+                  interests.map( inf =>{
+                  return<Select deletefieldint={this.deletefieldint} key={inf.id} {...inf}/>
+                })
+                }  
+              </div>
+              <div className="option">
+              <div className="optiontitle">
+                  <p>推荐：</p>
+                  {
+                    interestwords.map( inf =>{
+                      return<Interest addinterest={this.selectinterest} key={inf.id} {...inf}/>
+                      })
+                  }
+              </div>
+            </div>
+            <Link to="/personInfo">
+              <Button className="button1"
+              type={"primary"}
+              icon={<CheckCircleOutlined />}
+              size="middle"
+              shape={"round"}
+              style={saveStyle}
+              onClick={this.changeInfo}
+              >完成</Button>
+              <Button className="button2" 
+              icon = {<RollbackOutlined />}
+              type="primary"
+              size="middle"
+              shape={"round"}
+              style={{
+                  margin: '25px 40px 16px 30px',
+                  border: 'none',
+                  boxShadow: '4px 4px 15px 0 rgba(0,0,0,0.1)',
+              }}
+              >取消</Button>
+            </Link>
             </div>
           </div>
-          <Link to="/personInfo">
-            <Button className="button1"
-            type={"primary"}
-            icon={<CheckCircleOutlined />}
-            size="middle"
-            shape={"round"}
-            style={saveStyle}
-            >完成</Button>
-            <Button className="button2" 
-            icon = {<RollbackOutlined />}
-            type="primary"
-            size="middle"
-            shape={"round"}
-            style={{
-                margin: '25px 40px 16px 30px',
-                border: 'none',
-                boxShadow: '4px 4px 15px 0 rgba(0,0,0,0.1)',
-            }}
-            >取消</Button>
-          </Link>
-          </div>
-        </div>
+        </Form>
       </div>
     )
   }
--- src/pages/personalinf/Information/index.js
+++ src/pages/personalinf/Information/index.js
@@ -4,6 +4,8 @@ import Frame from '../Frame'
 import Edit from '../Edit'
 import { Route,Routes } from 'react-router-dom';
 import '../index.css';
+import axios from 'axios'
+
 
 export default class Information extends Component {
     state = {accountinfs:[
@@ -11,12 +13,12 @@ export default class Information extends Component {
         {id:'002',name:'门户ID：',value:'11223344' }
       ],
       personalinfs:[
-        {id:'003',name:'真实姓名：',value:'万光曦', },
-        {id:'004',name:'联系电话：',value:'110119120', },
-        {id:'005',name:'工作单位：',value:'北京航空航天大学', },
-        {id:'006',name:'E-mail：',value:'2690121951@qq.com', prefix:'* '},
-        {id:'007',name:'研究领域：',value:'机器学习', },
-        {id:'008',name:'我的兴趣词：',value:'机器学习', }
+        {id:'003',name:'真实姓名：',value:'', },
+        {id:'004',name:'联系电话：',value:'', },
+        {id:'005',name:'工作单位：',value:'', },
+        {id:'006',name:'E-mail：',value:'', prefix:'* '},
+        {id:'007',name:'研究领域：',value:'', },
+        {id:'008',name:'我的兴趣词：',value:'', }
       ],
       interestwords:[
         {id:'009',value:'中国式现代化'},
@@ -26,8 +28,52 @@ export default class Information extends Component {
         {id:'013',value:'软工2'},
         {id:'014',value:'区块链'}
       ],
-      scholar: false
+      scholar: true
     }
+
+  componentDidMount(){
+    axios.get('https://mock.apifox.cn/m1/1955876-0-default/personInfo').then(
+      response => {
+        const newPersonalinfs = this.state.personalinfs;
+        newPersonalinfs.map((item, index)=>{
+        if (item.id === '003'){
+          item.value = response.data.Rname;
+        }else if (item.id === '004'){
+          item.value = response.data.Rcontact;
+        }if (item.id === '005'){
+          item.value = response.data.Rinstitute;
+        }if (item.id === '006'){
+          item.value = response.data.Uemail;
+        }if (item.id === '007'){
+          const {Ufield} = response.data;
+          let out = '';
+          Ufield.map((item, index) => {
+            if (index < Ufield.length - 1){
+              out = `${out}${item}、`;
+            }else{
+              out = `${out}${item}`;
+            }
+          })
+          item.value = out;
+        }if (item.id === '008'){
+          const {Uinterest} = response.data;
+          let out = '';
+          Uinterest.map((item, index) => {
+            if (index < Uinterest.length - 1){
+              out = `${out}${item}、`;
+            }else{
+              out = `${out}${item}`;
+            }
+          })
+          item.value = out;
+        }
+      })
+      this.setState({personalinfs: newPersonalinfs});
+      console.log(response.data);
+      console.log(this.state)},
+      error => {console.log('失败了',error);}
+    )
+  }
 
   render() {
     const {accountinfs} = this.state
